744421d8a5851b43fa68949aa96c513579d6e00e
DavidSamerson <samerson10@gmail.com>
2018-04-11 09:42:17 -0300

    IONIC- LoginPage configuração

--- apiAppTeste/src/pages/login/login.ts
+++ apiAppTeste/src/pages/login/login.ts
@@ -1,12 +1,6 @@
+import { UserProvider } from './../../providers/user/user';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  model: User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private toast: ToastController, private userProvider: UserProvider) {
+
+                this.model = new User();
+                this.model.email = 'sydney@fife';
+                this.model.password = 'pistol';
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  login(){
+    this.userProvider.login(this.model.email, this.model.password)
+    .then((result: any) => {
+
+      this.toast.create({ message: 'Usuário Logado com sucesso. Token:'
+                        + result.token, position: 'bottom', duration: 3000}).present();
+                        //salvar o token para futuras requisições
+                        //redirecionar o usuário pra outra tela usando o navCtrl
+
+    })
+    .catch((error: any) => {
+
+      this.toast.create({ message: 'Erro ao efetuar o login. Erro:'
+                        + error.error, position: 'bottom', duration: 3000}).present();
+                        
+    });
   }
 
+}
+
+export class User {
+  email: string;
+  password: string;
 }

0aa8772526e0e121cdd145e5cffd08b1ba9a9db2
DavidSamerson <samerson10@gmail.com>
2018-04-11 13:05:24 -0300

    novos

--- apiAppTeste/src/pages/user-list/user-list.ts
+++ apiAppTeste/src/pages/user-list/user-list.ts
@@ -1,12 +1,7 @@
+import { UserProvider } from './../../providers/user/user';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
-/**
- * Generated class for the UserListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +10,97 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  users: any[];
+  page: number;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private toast: ToastController, private userProvider: UserProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserListPage');
+  ionViewDidEnter() {
+    this.users = [];
+    this.page = 1;
+  }
+
+  getAllUsers(page: number) {
+
+    this.userProvider.getAll(page)
+      .then((result: any) => {
+
+        //popular a variáveml com um for
+        
+        for(var i = 0; i < result.data.lenght; i++){
+          var user = result.data[i];
+          this.users.push(user);
+        }
+
+      })
+      .catch((error: any) => {
+        this.toast.create({message: 'Erro ao listar os usuários. Tipo de erro:'
+        + error.error, position: 'bottom', duration: 3000}).present();
+      
+      });
+  }
+
+  getUser(){
+    setTimeout(() => {
+      this.page += 1;
+      this.getAllUsers(this.page);
+    }, 500);
+  }
+
+  openUser(id: number) {
+    this.userProvider.get(id)
+      .then((result: any) => {
+        this.navCtrl.push('UserDetailPage', { user: result.data });
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Erro ao abrir o usuário:'
+        + error.error, position: 'bottom', duration: 3000 }).present();
+      });
+  }
+
+  openCreateUser() {
+    this.navCtrl.push('UserEditPage');
+  }
+
+  //editar o usuário chamando a mesma página de criação e jogando um objeto praq ele.
+  openEditUser(id: number){
+
+    this.userProvider.get(id)
+    .then((result: any) => {
+
+      this.navCtrl.push('UserEditPage', { user: result.data });
+
+    })
+    .catch((error: any) => {
+
+      this.toast.create({
+        message: 'Erro ao editar o usuário. Token:'
+                        + error.error, position: 'bottom', duration: 3000}).present();
+      });
+
+  }
+
+  deleteUser(user: any){
+
+    this.userProvider.remove(user.id)
+    .then((result: any) => {
+
+      let index = this.users.indexOf(user);
+      this.users.splice(index, 1);
+
+      this.toast.create({
+        message: 'Usuário deletado com sucesso.', position: 'bottom', duration: 3000}).present();
+
+    })
+    .catch((error: any) => {
+
+      this.toast.create({
+        message: 'Erro ao aqpagar o usuário. Erro:'
+                  + error.error, position: 'bottom', duration: 3000}).present();
+      });
+
   }
 
 }

974477b03ae8e0345a168f489e3b0c805e321501
DavidSamerson <samerson10@gmail.com>
2018-04-12 13:59:34 -0300

    novos SQL crud

--- SQLite/src/app/app.component.ts
+++ SQLite/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DatabaseProvider } from './../providers/database/database';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -9,14 +10,35 @@ import { TabsPage } from '../pages/tabs/tabs';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+  rootPage:any = null;
+
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, 
+              dbProvider : DatabaseProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      //método necessário para carregar o banco de dados antes da aplicação entrar.
+
+      dbProvider.createDatabase()
+        .then(() => {
+          this.openHomePage(splashScreen);
+        })
+        .catch(() => {
+          this.openHomePage(splashScreen);
+          //telas de erro
+        })
+
     });
+  }
+
+  private openHomePage( splashScreen : SplashScreen ) {
+    splashScreen.hide();
+    this.rootPage = TabsPage;
   }
 }
--- SQLite/src/providers/product/product.ts
+++ SQLite/src/providers/product/product.ts
@@ -1,17 +1,44 @@
-import { HttpClient } from '@angular/common/http';
+import { DatabaseProvider } from './../database/database';
+import { SQLiteObject } from '@ionic-native/sqlite';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the ProductProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ProductProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ProductProvider Provider');
+  constructor( private dbProvider : DatabaseProvider) {
+
+    console.log('Entrando no Product');
+
   }
+
+  public insert (product : Product) {
+
+    return this.dbProvider.getDB()
+      .then((db: SQLiteObject) => {
+        
+        let sql = 'insert into products (name, price, duedate, active, category_id) values (?, ?, ?, ?, ?)';
+        let data = [
+          product.name,
+          product.price,
+          product.duedate,
+          product.active,
+          product.category_id
+        ]; //variavel que vai pro banco
+
+      })
+      .catch((e) => console.error(e));
+
+  }
+
+}
+
+export class Product {
+
+  id: number;
+  name: string;
+  price: number;
+  duedate: Date;
+  active: boolean;
+  category_id: number;
 
 }

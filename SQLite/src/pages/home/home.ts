05a095d8da5ecaaae399bca9867c250744947251
DavidSamerson <samerson10@gmail.com>
2018-04-13 11:25:23 -0300

    novos

--- SQLite/src/pages/home/home.ts
+++ SQLite/src/pages/home/home.ts
@@ -1,5 +1,6 @@
+import { ProductProvider, Product } from './../../providers/product/product';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  products: any[] = [];
+  onlyInactives: boolean = false;
+  searchText: string = null;
 
+  constructor(public navCtrl: NavController, private toast: ToastController,
+                      private productProvider: ProductProvider) {  }
+
+                      ionViewDidEnter() {
+                        this.getAllProducts();
+                      }
+
+  getAllProducts() {
+    this.productProvider.getAll(!this.onlyInactives, this.searchText)
+      .then((result: any[]) => {
+        this.products = result;
+      })
+  }
+
+  addProduct() {
+    this.navCtrl.push('EditProductPage');
+  }
+
+  editProduct(id: number) {
+    this.navCtrl.push('EditProductPage', {id: id});
+  }
+
+  removeProduct(product: Product) {
+    this.productProvider.remove(product.id)
+      .then(() => {
+
+        var index = this.products.indexOf(product);
+        this.products.splice(index, 1);
+        this.toast.create({ message: 'Produto Removido. ', duration: 3000, position: 'bottom' }).present();
+
+      })
+  }
+
+  filterProducts (ev: any) {
+    this.getAllProducts();
   }
 
 }
--- SQLite/src/providers/category/category.ts
+++ SQLite/src/providers/category/category.ts
@@ -1,17 +1,49 @@
-import { HttpClient } from '@angular/common/http';
+import { DatabaseProvider } from './../database/database';
+import { SQLiteObject } from '@ionic-native/sqlite';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the CategoryProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class CategoryProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello CategoryProvider Provider');
+  constructor( private dbProvider : DatabaseProvider) {
+    console.log('Olá Providers de Categoria');
+  }
+
+  public getAll() {
+
+    return this.dbProvider.getDB()
+      .then((db: SQLiteObject) => {
+
+        let sql = 'select * from categories';
+
+
+        return db.executeSql(sql, [])
+              .then((data: any) => {
+
+                if(data.rows.length > 0){
+
+                    // percorrer o resultado e adicionar em um objeto
+                  let categories : any [] = [];
+
+                  for(var i = 0; i < data.rows.length; i++) {
+
+                      var category = data.rows.item(i);
+
+                      categories.push(category);
+                  }
+
+                  return categories;
+
+                } else {
+
+                  return [];
+                
+                }
+              })
+              .catch((e) => console.error(e)); //executa um SQL
+
+      })
+      .catch((e) => console.error(e));
   }
 
 }

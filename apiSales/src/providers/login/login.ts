3085d83356eae78e473036b680ccad969f41234d
David Samerson <samerson10@gmail.com>
2018-04-16 20:50:37 -0300

    Novo

--- apiSales/src/providers/login/login.ts
+++ apiSales/src/providers/login/login.ts
@@ -20,6 +20,15 @@ export class LoginProvider {
 
     let headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    let options = new RequestOptions({ headers: headers });
+
+    //PROCURAR DOCUMENTAçÂO HTTP DO IONIC
+
+    //solução 1 -----------------------
+    // let data = "grant_type=password"+
+    //       "&client_id=3MVG9AzPSkglhtps5GKCVvVsZxbvTiThV6KhFCwSatL6665Syr5ZKsZCRFZDK1Yu3K0xjPIWUHO7FciqL2kr7"+
+    //       "&client_secret=8770981992942896379"+
+    //       "&username=" + email + "&password=" + password;
 
       // const headers = new HttpHeaders({
       //   'Content-Type': 'application/json; charset=utf-8',
@@ -27,36 +36,43 @@ export class LoginProvider {
       //   user_id: '90'
       // });
 
-    let body = new FormData();
-        body.append('grant_type','password');
-        body.append('client_id','3MVG9AzPSkglhtps5GKCVvVsZxbvTiThV6KhFCwSatL6665Syr5ZKsZCRFZDK1Yu3K0xjPIWUHO7FciqL2kr7');
-        body.append('client_secret','8770981992942896379');
-        body.append('username', email);
-        body.append('password', password);
+    //solução 2 ---------------------------------------
+    // let body = new FormData();
+    //     body.append('grant_type','password');
+    //     body.append('client_id','3MVG9AzPSkglhtps5GKCVvVsZxbvTiThV6KhFCwSatL6665Syr5ZKsZCRFZDK1Yu3K0xjPIWUHO7FciqL2kr7');
+    //     body.append('client_secret','8770981992942896379');
+    //     body.append('username', email);
+    //     body.append('password', password);
 
-    let options = new RequestOptions({ headers: headers });
 
+    var body = JSON.stringify({
+    grant_type: 'password', 
+    client_id: '3MVG9AzPSkglhtps5GKCVvVsZxbvTiThV6KhFCwSatL6665Syr5ZKsZCRFZDK1Yu3K0xjPIWUHO7FciqL2kr7',
+    client_secret: '8770981992942896379',
+    username: email,
+    password: password});
+ 
     console.log(body);
 
     return new Promise((resolve, reject) => {
 
-      this.http.post(this.API_URL, body, options)
-      .map(res => res.json())
-      .subscribe(
-          data => {
-            console.log(data);
-          },
-          error => {
-            console.log("Error! Message: ", error);
+      this.http.post(this.API_URL, body , options)
+        .subscribe((result: any) => {
+          console.log("Requisição efetuada");
+          resolve(result.json());
+        },
+          (error) => {
+            reject(error.json());
+            console.log("Erro na requisição");
           });
-        // .subscribe((result: any) => {
-        //   console.log("Requisição efetuada");
-        //   resolve(result.json());
-        // },
-        //   (error) => {
-        //     reject(error.json());
-        //     console.log("Erro na requisição");
-        //   });
+        // .map(res => res.json())
+        // .subscribe(
+        //     data => {
+        //       console.log(data);
+        //     },
+        //     error => {
+        //       console.log("Error! Message: ", error);
+        //     });
     });
   }
 

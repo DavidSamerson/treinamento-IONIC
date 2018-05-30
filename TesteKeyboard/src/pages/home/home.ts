29a4372d793af08d6e57483b585d6e63c704650f
David Samerson <samerson10@gmail.com>
2018-05-30 16:51:09 -0300

    Teste de FormBuilder

--- TesteKeyboard/src/pages/home/home.ts
+++ TesteKeyboard/src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, Input, ViewChild} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  //PASSO 1 ------------------------------
+  //declarar uma ViewChild com o nome Input
+  @ViewChild('input') myInput ;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewLoaded() {
+      this.myInput.setFocus();
+ }
+
+ clickk(){
+    this.myInput.setFocus();
+ }
 
 }

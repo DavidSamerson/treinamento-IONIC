9ea3ed24cff30e48aff4695e74417294b89d097c
David Samerson <samerson10@gmail.com>
2018-08-30 16:48:17 -0300

    novo projeto - treinando o Lazy Load

---
+++ ionicFirstApp/src/pages/about/about.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the AboutPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({
+  name: 'AboutPage',
+  segment: 'pagina-sobre/:id',
+  defaultHistory : ['HomePage']
+})
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.navParams.get('id');
+  }
+
+}
